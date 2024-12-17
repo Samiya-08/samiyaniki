72106d8586ea1d2f59cab5340da8e806745daf91
samiya-08 <samiyamahmudiy@gmail.com>
2024-12-17 21:32:50 +0500

    building new project

---
+++ samiyanikimi/main.py
@@ -0,0 +1,10 @@
+"""
+echo "# samiyaniki" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Samiya-08/samiyaniki.git
+git push -u origin main
+"""
+
